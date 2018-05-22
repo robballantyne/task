8be8321b3bcb5100b4e9c4aebbc0e2c427b3ea1f
RobBallantyne <rob@robballantyne.co.uk>
2018-05-22 16:41:54 +0100

    move API logic to artisan command and retain update logic in model

--- app/Beer.php
+++ app/Beer.php
@@ -2,9 +2,44 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Beer extends Model
 {
-    //
+    protected $guarded = [];
+
+
+    public function updateBeer($beer)
+    {
+        $beer = $this->updateOrCreate(
+            [
+                'name'              => $beer->name,
+            ],
+            [
+                'tagline'           => $beer->tagline,
+                // API returns string date so convert to proper SQL date format
+                'first_brewed'      => Carbon::createFromFormat('m/Y', $beer->first_brewed),
+                'description'       => $beer->description,
+                'image_url'         => $beer->image_url,
+                'abv'               => $beer->abv,
+                'ibu'               => $beer->ibu,
+                'target_fg'         => $beer->target_fg,
+                'target_og'         => $beer->target_og,
+                'ebc'               => $beer->ebc,
+                'srm'               => $beer->srm,
+                'ph'                => $beer->ph,
+                'attenuation_level' => $beer->attenuation_level,
+                'volume'            => json_encode($beer->volume),
+                'boil_volume'       => json_encode($beer->boil_volume),
+                'method'            => json_encode($beer->method),
+                'ingredients'       => json_encode($beer->ingredients),
+                'food_pairing'      => json_encode($beer->food_pairing),
+                'brewers_tips'      => $beer->brewers_tips,
+                'contributed_by'    => $beer->contributed_by
+            ]
+        );
+
+        return $beer;
+    }
 }
