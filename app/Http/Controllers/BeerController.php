f2abdf9e827b4373bddf46d6752129c42141a319
RobBallantyne <rob@robballantyne.co.uk>
2018-05-23 11:46:43 +0100

    Add some logic for searching beers by name

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -9,26 +9,86 @@ use App\Http\Requests;
 
 class BeerController extends Controller
 {
+    // Injecting the beer model. We will use scopes and possibly adopt the repository pattern.
+    protected $beerModel;
+
+    // a collection of beers
 	private $beers = [];
 
-	public function __construct()
+	// a single beer
+	private $beer = null;
+
+	// a search query
+	private $search = false;
+
+	// inject the beer model and the request
+	public function __construct(Beer $beerModel, Request $request)
 	{
-		$this->beers = Beer::all();
-		// TODO create paginator and formatting
+	    $this->request = $request;
+	    $this->beerModel = $beerModel;
 	}
 
-	public function index(Request $request)
+    /**
+     * Index returns all the beers
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+	public function index()
 	{
+        // TODO create paginator and formatting
+	    $this->beers = $this->beerModel->all();
+
 		return view('beer.index', [
 			'beers' => $this->beers
 		]);
 		// TODO formatting
 	}
 
+    /**
+     * Return a specific beer by id
+     *
+     * @param $id
+     */
 	public function show($id)
 	{
-		$beer = Beer::find($id);
+		$beer = $this->beerModel->find($id);
 
 		dd($beer);
 	}
+
+    /**
+     * Search for beers by name
+     */
+	public function search()
+    {
+        $this->search = $this->request->post("search");
+
+        // If there is a search query passed, search.
+        if ($this->search) {
+            $this->beers = $this->beerModel->search($this->search)->get();
+        } else {
+            // No query passed so return everything.
+            $this->beers = $this->beerModel->all();
+        }
+
+        dd($this->beers);
+    }
+
+    /**
+     * Return a random beer from the database
+     */
+    public function random()
+    {
+        $this->beer = $this->beerModel->inRandomOrder()->first();
+
+        dd($this->beer);
+    }
+
+    /**
+     * Return only the beers which have been marked as favourites
+     */
+    public function favourites()
+    {
+        // Implement this
+    }
 }
