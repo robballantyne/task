42f15045d3135c541d9cda723e6d3ee8b67af70f
RobBallantyne <rob@robballantyne.co.uk>
2018-05-23 21:39:57 +0100

    defer logic to subviews

--- resources/views/beer/index.blade.php
+++ resources/views/beer/index.blade.php
@@ -1,30 +1,13 @@
 @extends('layout')
 
 @section('content')
+    @if ($list)
+        @include('beer.listloop')
 
-    <div class="container beers-grid">
-        <div class="row">
-            @foreach($beers as $beer)
-                <div class="col-sm-6 col-lg-4">
-                    <div class="beer-wrapper">
-                        <a href="{{ route('beer.show', $beer->id) }}">
-                        <img class="img-responsive" src="{{ $beer->image_url }}">
-                        </a>
-                        <div class="info">
-                            <p class="title"><a href="{{ route('beer.show', $beer->id) }}">{{ $beer->name }}</a></p>
-                        <p class="tagline">{{ $beer->tagline }}</p>
-                        </div>
+    @else
+        @include('beer.gridloop')
+    @endif
 
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-
-    <div class="container pagination-container">
-        <div class="row">
-            {{$beers->links()}}
-        </div>
-    </div>
+    @include('beer.pagination')
 
 @endsection
