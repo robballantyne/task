3ea7acfc8b529b9907179dd86fa5f8b6faa1214c
RobBallantyne <rob@robballantyne.co.uk>
2018-05-23 15:08:22 +0100

    remove search form as now in layout.
    add very basic scaffolding for grid view.

--- resources/views/beer/index.blade.php
+++ resources/views/beer/index.blade.php
@@ -1,34 +1,30 @@
 @extends('layout')
 
 @section('content')
-    <form action="{{ route('beer.search') }}" method="POST">
-        <div class="container">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <input name="search" type="text" class="form-control" placeholder="Search...">
-                            {{ csrf_field() }}
-                        </div>
-                        <div class="col-md-2">
-                            <button class="btn btn-block btn-primary"><span class="glyphicon glyphicon-search"></span> Search</button>
-                        </div>
-                        <div class="col-md-2">
-                            <a href="{{ route('beer.random') }}" class="btn btn-block btn-default"><span class="glyphicon glyphicon-refresh"></span> Random</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
 
-    <div class="container">
+    <div class="container beers-grid">
         <div class="row">
             @foreach($beers as $beer)
-                <div class="col-md-4">
-                    <p><a href="{{ route('beer.show', $beer->id) }}">{{ $beer->name }}</a></p>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="beer-wrapper">
+                        <a href="{{ route('beer.show', $beer->id) }}">
+                        <img class="img-responsive" src="{{ $beer->image_url }}">
+                        </a>
+                        <div class="info">
+                            <p class="title"><a href="{{ route('beer.show', $beer->id) }}">{{ $beer->name }}</a></p>
+                        <p class="tagline">{{ $beer->tagline }}</p>
+                        </div>
+
+                    </div>
                 </div>
             @endforeach
         </div>
     </div>
+
+    <div class="container pagination-container">
+        <div class="row">
+            {{$beers->links()}}
+        </div>
+    </div>
+
 @endsection
