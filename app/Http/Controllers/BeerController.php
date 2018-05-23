42ac463205beade845deb99ab9461ea27cf89ac1
RobBallantyne <rob@robballantyne.co.uk>
2018-05-23 22:20:03 +0100

    change search key to 'query'

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -6,6 +6,7 @@ use App\Beer;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
 
 class BeerController extends Controller
 {
@@ -21,11 +22,15 @@ class BeerController extends Controller
 	// a search query
 	private $search = false;
 
+	private $persistQueryString = [];
+
 	// inject the beer model and the request
 	public function __construct(Beer $beerModel, Request $request)
 	{
 	    $this->request = $request;
 	    $this->beerModel = $beerModel;
+	    // Paginator will drop the query string unless we specifically opt to keep params
+	    $this->persistQueryString = Input::only(['view', 'query']);
 	}
 
     /**
@@ -35,12 +40,17 @@ class BeerController extends Controller
      */
 	public function index()
 	{
-        // TODO create paginator and formatting
-	    $this->beers = $this->beerModel->all();
+        // Paginate 12 by default. Nicely divides into even numbers per row
+	    $this->beers = $this->beerModel->paginate(12)
+            ->appends($this->persistQueryString);
 
+	    // grid view
 		return view('beer.index', [
-			'beers' => $this->beers
+			'beers' => $this->beers,
+            'list'  => $this->isListView()
 		]);
+
+		// list view
 		// TODO formatting
 	}
 
@@ -61,17 +71,21 @@ class BeerController extends Controller
      */
 	public function search()
     {
-        $this->search = $this->request->post("search");
+        $this->search = $this->request->post("query");
 
         // If there is a search query passed, search.
         if ($this->search) {
-            $this->beers = $this->beerModel->search($this->search)->get();
+            $this->beers = $this->beerModel->search($this->search)->paginate(12);
         } else {
             // No query passed so return everything.
-            $this->beers = $this->beerModel->all();
+            $this->beers = $this->beerModel->paginate(12);
         }
 
-        dd($this->beers);
+        return view('beer.search', [
+            'query' => $this->search,
+            'beers' => $this->beers,
+            'list'  => $this->isListView()
+        ]);
     }
 
     /**
@@ -90,5 +104,19 @@ class BeerController extends Controller
     public function favourites()
     {
         // Implement this
+    }
+
+    /**
+     * Determine whether our url contains query param 'view' value 'list'.
+     *
+     * @return bool
+     */
+    private function isListView()
+    {
+        // TODO we will store preferred view in session rather than relying on querystring
+        if (strtolower($this->request->get('view')) == 'list') {
+            return true;
+        }
+        return false;
     }
 }
