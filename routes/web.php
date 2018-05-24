4b569cc656cc261b07a297db5d2c0a3583604c5a
RobBallantyne <rob@robballantyne.co.uk>
2018-05-24 11:06:56 +0100

    do search on get method rather than post. Allows for linking to and easy pagination

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::group([ 'middleware' => [ 'web' ] ], function()
 {
 	Route::get('/', [ 'uses' => 'BeerController@index' ]);
 	Route::get('beer/random', [ 'uses' => 'BeerController@random', 'as' => 'beer.random' ]);
-	Route::post('beer/search', [ 'uses' => 'BeerController@search', 'as' => 'beer.search' ]);
+	// Changed search this to get for simpler pagination
+	Route::get('beer/search', [ 'uses' => 'BeerController@search', 'as' => 'beer.search' ]);
 	Route::resource('beer', 'BeerController', [ 'only' => [ 'index', 'show' ] ]);
 });
